ed19497ad015e9dd6bba73452ebc5d2c945b99ea
Andrea Viviani <andrea.viviani89@gmail.com>
2022-03-31 23:34:48 +0200

    update

--- stream_visual.py
+++ stream_visual.py
@@ -6,45 +6,35 @@ import plotly.express as px
 from PIL import Image
 
 df = pd.read_csv("C:/Users/andre/Documents/Strive_repository/1st-Build-Week/Data/Resturantdata.csv")
+df_pub = pd.read_csv("C:/Users/andre/Documents/Strive_repository/1st-Build-Week/Data/Pubdata.csv")
+df_hotel = pd.read_csv("C:/Users/andre/Documents/Strive_repository/1st-Build-Week/Data/Hoteldata.csv")
 
 
 
 data_select = st.sidebar.selectbox("Select the data you want to see", ("Home", "Restaurants", "Hotels", "Pubs"))
 
-
+#   Homepage
 if data_select == "Home":
+
 
     st.title("Welcome")
     st.header('Team member')
     st.markdown('* *Andrea*')
     st.markdown('* *Eunice*')
 
+    st.title('Accommodation, Restaurants & Drink Services in Berlin')
+    st.header('Why accommodation, food, and drink services industry?')
+    st.markdown("""The accommodation industry and the food and drink services industry have faced unprecedented change due to the coronavirus (COVID-19) pandemic.
+    Up until this moment, the accommodation, food, and drink services industry have been seeing consistent year-over-year growth in all its sectors.
+    Yet, while the pandemic has proved to be a difficult time for these industries, it is still predicted to see growth.""")
 
 #   Restaurants
-
 elif data_select == "Restaurants":
 
     st.title("Restaurant insights")
-
-
-    #   First graph
-
-    # st.subheader('Graph Title - to do')       --> Made a better version with plotly
-
-    # fig1 = df['Neighbourhoods'].value_counts()
-    # plt.xlabel('Locations')
-    # plt.ylabel('Counts')
-
-    # st.bar_chart(fig1)
-
     
+    #   Graph n.1
 
-    # st.subheader('Graph Title - to do')
-    # df_neigh = df["Neighbourhoods"].value_counts()
-    # go = px.bar(df_neigh, x='Neighbourhoods')
-    # st.write(go)
-    # st.text('Little description here')
-    
     st.subheader('Graph Title - to do')
     fig=px.bar(df['Neighbourhoods'].value_counts(), height=600, width=800, labels={
         "value": "Number of restaurants per neighbourhood",
@@ -53,9 +43,27 @@ elif data_select == "Restaurants":
     st.write(fig)
     
     st.text('Little description here')
+
+    #   Graph n.2
+
+    fig = px.bar(df['Prices'].value_counts(ascending=True), orientation='h', height=650, width=850, labels={
+            "value": "Number of Restaurants with respective price range",
+            "index": "Price range (in euros)"
+    },
+    title='Price range of restaurants in Berlin')
+    st.write(fig)    
     
-    
-    #   Second graph
+    #   Graph n.3
+
+    st.subheader('Graph Title - to do')
+    df_type = px.bar(df["Type_of_resturant"].value_counts(ascending=True).nlargest(10), height=600, width=800, labels={
+        "value": "Type of restaurants",
+        "index": "Categories"
+    })
+    st.write(df_type)
+    st.text('Little description here')
+
+    #   Graph n.4 
     
     st.subheader('Need to do it faincier')
 
@@ -71,35 +79,98 @@ elif data_select == "Restaurants":
     st.write(test)
     st.text('Little description here')
 
+    #   Graph n.5
 
-    #   Third graph
     st.subheader('Need to do it faincier')
     df[['Type_of_resturant','Ratings']].groupby('Type_of_resturant').value_counts().nlargest(10). plot(kind='barh')
 
     plt.xlabel('Counts')
     plt.title('Most prefered type of restaurant by rating')
     st.pyplot()
-
-
-
-
-    # Graph
     st.text('Little description here')
 
 
 
-    fig = px.bar(df['Prices'].value_counts(ascending=True), orientation='h', height=650, width=850, labels={
-            "value": "Number of Restaurants with respective price range",
-            "index": "Price range (in euros)"
-    },
-    title='Price range of restaurants in Berlin')
-    st.write(fig)
+#   Hotels
+
+elif data_select == "Hotels":
+
+    st.title("Hotels insights")
 
 
+#   Graph n.1
 
     st.subheader('Graph Title - to do')
-    df_type = px.bar(df["Type_of_resturant"].value_counts(ascending=True).nlargest(10), height=600, width=800, labels={
-        "value": "Type of restaurants",
+    fig=px.bar(df_hotel['Neighbourhoods'].value_counts(), orientation='h', height=700, width=850, labels={
+        "value": "Number of hotels per neighbourhood",
+        "index": "Neighbourhoods"
+    })
+    st.write(fig)
+
+    st.text('Little description here')
+
+
+#   Graph n.2
+
+    fig = px.bar(df_hotel['Prices'].value_counts(ascending=True), orientation='h', height=650, width=850, labels={
+        "value": "Number of hotels with respective price range",
+        "index": "Price range (in euros)"
+    },
+    title='Price range of hotels in Berlin')
+    st.write(fig)   
+
+    st.text('Little description here')
+
+
+#   Graph n.3
+
+    st.subheader('Graph Title - to do')
+    df_type = px.bar(df_hotel["Type_of_hotels"].value_counts(ascending=True).nlargest(10), height=600, width=800, labels={
+        "value": "Type of hotels",
+        "index": "Categories"
+    })
+    st.write(df_type)
+    st.text('Little description here')
+
+
+
+
+#   Pubs
+
+elif data_select == "Pubs":
+
+    st.title("Pubs insights")
+
+
+#   Graph n.1
+
+    st.subheader('Graph Title - to do')
+    fig=px.bar(df_pub['Neighbourhoods'].value_counts(), orientation='h', height=700, width=850, labels={
+        "value": "Number of pubs per neighbourhood",
+        "index": "Neighbourhoods"
+    })
+    st.write(fig)
+
+    st.text('Little description here')
+
+
+#   Graph n.2
+
+    fig = px.bar(df_pub['Prices'].value_counts(ascending=True), orientation='h', height=650, width=850, labels={
+        "value": "Number of pubs with respective price range",
+        "index": "Price range (in euros)"
+    },
+    title='Price range of pubs in Berlin')
+    st.write(fig)   
+
+    st.text('Little description here')
+
+
+#   Graph n.3
+
+    st.subheader('Graph Title - to do')
+    df_type = px.bar(df_pub["Type_of_pubs"].value_counts(ascending=True).nlargest(10), height=600, width=800, labels={
+        "value": "Type of pubs",
         "index": "Categories"
     })
     st.write(df_type)
@@ -110,36 +181,6 @@ elif data_select == "Restaurants":
 
 
 
-    # fig2 = df[['Neighbourhoods','Ratings']].groupby('Ratings').value_counts()
-    # go = px.bar(fig2, x='Neighbourhoods', y='Ratings')
-    # st.write(fig2)
-
-
-
-
-
-    # fig2 = df[['Neighbourhoods','Ratings']].groupby('Ratings').value_counts()
-    # plt.xlabel
-    # plt.ylabel
-
-
-    # st.bar_chart(fig2)
-
-
-
-
-
-
-    # Need to fix it
-    # fig = px.scatter(data_frame=df, x = 'Number_of_reviews', y = 'Ratings', color = 'Index', template = 'seaborn',
-    #     title = 'Test' )
-
-
-#   Hotels
-
-elif data_select == "Hotels":
-
-    st.title("Hotels insights")
 
 
 
@@ -147,39 +188,3 @@ elif data_select == "Hotels":
 
 
 
-
-
-
-#       '''first way'''
-
-# sidebar = st.sidebar
-# header = st.container()
-
-# with sidebar:
-#     st.header('Team member')
-#     st.markdown('* **Andrea**')
-#     st.markdown('* **Eunice**')
-
-
-#     st.header('Data analyze')
-#     st.markdown('* **1:** Yelp web scraping.')
-
-
-
-#       '''second way'''
-
-# st.title("Welcome to Yelp web scraping challenge")
-# st.subheader("Let's have a look")
-
-
-# headers = ['Home', 'Restaurants', 'Hotels', 'Pubs']
-# df = pd.read_csv(r'C:/Users/andre/Documents/Strive_repository/1st-Build-Week/Data/Resturantdata.csv', header=0)
-
-# if st.button('Show data'):
-#     st.dataframe(df)
-
-
-# book_input = st.text_input('Names')
-# df1 = df.loc[df['Names'].str.contains(book_input, flags=re.IGNORECASE)]
-# if len(book_input) > 0:
-#     st.dataframe(df1)
